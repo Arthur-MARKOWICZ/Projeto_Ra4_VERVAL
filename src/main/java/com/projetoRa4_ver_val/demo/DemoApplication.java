876da1a381708cb5f21202657a99eecd93e05736
Your Name <arthurlopes25072005@gmail.com>
2025-11-04 10:32:09 -0300

    feat:criando a base do projeto

---
+++ src/main/java/com/projetoRa4_ver_val/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.projetoRa4_ver_val.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoRa4_ver_val/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoRa4_ver_val.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
