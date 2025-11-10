025d8b81351edcdc945d757131e2d031954b5391
Luigi Morgenstern Milani <luigimilani2003@outlook.com>
2025-11-10 12:27:24 -0300

    fazendo controller  de deletar livro e usuario

--- src/main/java/com/projetoRa4_ver_val/demo/livro/LivroController.java
+++ src/main/java/com/projetoRa4_ver_val/demo/livro/LivroController.java
@@ -1,6 +1,5 @@
 package com.projetoRa4_ver_val.demo.livro;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +16,13 @@ public class LivroController {
     @PostMapping
     public ResponseEntity<Livro> cadastro(@RequestBody LivroCadastroDto dto){
         var livro = service.cadastro(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(livro);
+        return ResponseEntity.status(201).body(livro);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletar(new LivroDeletarDto(id));
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{idLivro}/emprestar/{idUsuario}")
@@ -30,9 +35,8 @@ public class LivroController {
     }
 
     @PostMapping("/{idLivro}/devolver")
-    public ResponseEntity<Livro> devolverLivro(@PathVariable Long idLivro) {
+    public ResponseEntity<Livro> devolver(@PathVariable Long idLivro) {
         var livro = service.devolverLivro(idLivro);
         return ResponseEntity.ok(livro);
     }
 }
-
