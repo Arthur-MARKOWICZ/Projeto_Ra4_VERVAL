21009a2fd771e3ca3a0282ff0fe388838abdfed1
André Benato <andregbenato@outlook.com>
2025-11-10 12:14:15 -0300

    Criando controller para Emprestar e Devolver livro

--- src/main/java/com/projetoRa4_ver_val/demo/livro/LivroController.java
+++ src/main/java/com/projetoRa4_ver_val/demo/livro/LivroController.java
@@ -2,21 +2,37 @@ package com.projetoRa4_ver_val.demo.livro;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/livros")
 public class LivroController {
+
     private final LivroService service;
 
     public LivroController(LivroService service) {
         this.service = service;
     }
+
     @PostMapping
-    public ResponseEntity<Livro> cadastro(LivroCadastroDto dto){
+    public ResponseEntity<Livro> cadastro(@RequestBody LivroCadastroDto dto){
         var livro = service.cadastro(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(livro);
     }
+
+    @PostMapping("/{idLivro}/emprestar/{idUsuario}")
+    public ResponseEntity<Livro> emprestar(
+            @PathVariable Long idLivro,
+            @PathVariable Long idUsuario
+    ) {
+        var livro = service.emprestarLivro(idLivro, idUsuario);
+        return ResponseEntity.ok(livro);
+    }
+
+    @PostMapping("/{idLivro}/devolver")
+    public ResponseEntity<Livro> devolverLivro(@PathVariable Long idLivro) {
+        var livro = service.devolverLivro(idLivro);
+        return ResponseEntity.ok(livro);
+    }
 }
+
